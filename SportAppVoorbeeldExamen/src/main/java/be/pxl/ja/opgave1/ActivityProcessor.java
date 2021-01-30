75068f86c6dffa882201f74059a09b318356cb04
JorisBouduinPXL <44054273+JorisBouduinPXL@users.noreply.github.com>
2021-01-30 12:11:52 +0100

    Add files via upload
    added sportapp

---
+++ SportAppVoorbeeldExamen/src/main/java/be/pxl/ja/opgave1/ActivityProcessor.java
@@ -0,0 +1,21 @@
+package be.pxl.ja.opgave1;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityProcessor {
+
+	private CustomerRepository customerRepository;
+	
+	public ActivityProcessor(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
+	public List<Activity> processActivities(Path activityFile, Path errorFile) {
+		List<Activity> activities = new ArrayList<>();
+		activities.read
+		// TODO: implement this method
+		return null;
+	}
+}
