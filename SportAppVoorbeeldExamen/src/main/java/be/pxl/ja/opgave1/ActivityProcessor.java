cab45cc602a2aac21609e2504af5e0d1f94aec86
JorisBouduinPXL <44054273+JorisBouduinPXL@users.noreply.github.com>
2021-01-31 11:34:31 +0100

    ActivityProcessor stravamapper, endomondomapper

--- SportAppVoorbeeldExamen/src/main/java/be/pxl/ja/opgave1/ActivityProcessor.java
+++ SportAppVoorbeeldExamen/src/main/java/be/pxl/ja/opgave1/ActivityProcessor.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 public class ActivityProcessor {
 
 	private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
+	private static final DateTimeFormatter STRAVAFORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private CustomerRepository customerRepository;
 	
 	public ActivityProcessor(CustomerRepository customerRepository) {
@@ -22,29 +23,49 @@ public class ActivityProcessor {
 	
 	public List<Activity> processActivities(Path activityFile, Path errorFile) {
 		// TODO: implement this method
-		activityReader(activityFile);
-		return null;
+		List<Activity> activities = new ArrayList<>();
+		return activities;
 	}
 
+
+
 	public List<Activity> activityReader(Path path){
+		List<Activity> list = new ArrayList<>();
 		try(BufferedReader reader = Files.newBufferedReader(path)){
-			String line = null;
-			while ((line = reader.readLine()) != null){
-				System.out.println(line);
+			reader.readLine();
+			String line = reader.readLine();
+			while (line != null){
+				if (path.toString().toUpperCase(Locale.ROOT).contains("ENDOMONDO")){
+					list.add(mapLineToEndomondoActivity(line));
+					line = reader.readLine();
+				}
+				if (path.toString().toUpperCase(Locale.ROOT).contains("STRAVA")){
+					list.add(mapLineToSravaActivity(line));
+					line = reader.readLine();
+				}
 			}
 		}catch (IOException e){
 			e.printStackTrace();
 		}
-
 		// TODO: implement this method
-		return null;
+		return list;
+	}
+
+	private Activity mapLineToSravaActivity(String line) {
+		Activity activity = new Activity();
+		String[] parts = line.split("[ ;]");
+		activity.setCustomerNumber(parts[2]);
+		activity.setActivityType(ActivityType.valueOf(parts[4].toUpperCase(Locale.ROOT)));
+		activity.setActivityDate(LocalDate.parse(parts[3], STRAVAFORMAT));
+		activity.setDistance(Double.parseDouble(parts[5]));
+		return activity;
 	}
 
 	//
 	public static Activity mapLineToEndomondoActivity(String line){
 		Activity activity = new Activity();
 		String[] parts = line.split(";");
-		activity.setActivityDate(LocalDate.parse(parts[1], FORMAT));
+		activity.setActivityDate(LocalDate.parse(parts[0], FORMAT));
 		activity.setCustomerNumber(parts[1]);
 		activity.setActivityType(ActivityType.valueOf(parts[2].toUpperCase(Locale.ROOT)));
 		activity.setDistance(Double.parseDouble(parts[3]));
