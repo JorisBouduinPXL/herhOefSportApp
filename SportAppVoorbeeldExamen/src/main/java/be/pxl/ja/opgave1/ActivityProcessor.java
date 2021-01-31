eadd7f8ba15b1b6434e57caef5f39e38a91f6118
JorisBouduinPXL <44054273+JorisBouduinPXL@users.noreply.github.com>
2021-01-31 11:00:26 +0100

    added endomondo mapper

--- SportAppVoorbeeldExamen/src/main/java/be/pxl/ja/opgave1/ActivityProcessor.java
+++ SportAppVoorbeeldExamen/src/main/java/be/pxl/ja/opgave1/ActivityProcessor.java
@@ -1,11 +1,19 @@
 package be.pxl.ja.opgave1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ActivityProcessor {
 
+	private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
 	private CustomerRepository customerRepository;
 	
 	public ActivityProcessor(CustomerRepository customerRepository) {
@@ -13,9 +21,33 @@ public class ActivityProcessor {
 	}
 	
 	public List<Activity> processActivities(Path activityFile, Path errorFile) {
-		List<Activity> activities = new ArrayList<>();
-//		activities.read
+		// TODO: implement this method
+		activityReader(activityFile);
+		return null;
+	}
+
+	public List<Activity> activityReader(Path path){
+		try(BufferedReader reader = Files.newBufferedReader(path)){
+			String line = null;
+			while ((line = reader.readLine()) != null){
+				System.out.println(line);
+			}
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+
 		// TODO: implement this method
 		return null;
+	}
+
+	//
+	public static Activity mapLineToEndomondoActivity(String line){
+		Activity activity = new Activity();
+		String[] parts = line.split(";");
+		activity.setActivityDate(LocalDate.parse(parts[1], FORMAT));
+		activity.setCustomerNumber(parts[1]);
+		activity.setActivityType(ActivityType.valueOf(parts[2].toUpperCase(Locale.ROOT)));
+		activity.setDistance(Double.parseDouble(parts[3]));
+		return activity;
 	}
 }
